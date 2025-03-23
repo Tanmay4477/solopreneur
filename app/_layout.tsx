0b42d5d28dac25666100453469140ec3f9a36bbb
Tanmay Jain <tanmayjain4477@gmail.com>
2025-03-23 15:25:21 +0530

    init: splash screen logic implemented

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,41 @@
-import { Stack } from "expo-router";
-import "../global.css"
+import { useCallback, useEffect, useState } from 'react';
+import '../global.css'
+import * as SplashScreen from 'expo-splash-screen'
+import { View } from 'react-native';
+import { Stack } from 'expo-router';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      {/* <Stack.Screen name="about" options={{ title: 'About' }} /> */}
-    </Stack>
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <Stack />
+    </View>
   );
 }
